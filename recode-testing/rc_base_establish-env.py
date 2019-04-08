fd0b1e95c5440ae7e3687182e42f409c2e3acd82
Greg Focaccio <public.focaccio@gmail.com>
2019-04-08 04:36:05 -0700

    module baselines validation testing 1

---
+++ recode-testing/rc_base_establish-env.py
@@ -0,0 +1,35 @@
+#rc=recode i.e. not gafc / not greg (good) af (areaf) code
+# test editing from git hub bowser
+from __future__ import print_function
+import argparse
+import commands
+import configparser
+import glob
+import os
+import paramiko
+import pexpect
+import re
+import select
+import shutil
+import signal
+import string
+import sys
+import syslog
+import time
+import traceback
+import io
+# necessary for fnxs
+import netlib
+
+
+
+from binascii import *
+from cmd import *
+from ftplib import *
+from netmiko import *
+from optparse import *
+from paramiko import *
+from paramiko.ssh_exception import *
+from paramiko.py3compat import *
+# from pyRouterLib import *
+from StringIO import StringIO
